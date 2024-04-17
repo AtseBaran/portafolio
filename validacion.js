1775c4a14b7de68f1de15db7d139ac54dbca10ed
Esteban Loria <eavwsa@gmail.com>
2024-04-17 16:31:56 -0600

    new file:   assets/profile.jpeg 	modified:   index.html 	modified:   style.css 	modified:   validacion.js

--- validacion.js
+++ validacion.js
@@ -1,1 +1,30 @@
 //Haz tú validación en javascript acá
+
+function validar(){
+    var nombre = document.forms["form"]["nombre"].value;
+    var email = document.forms["form"]["email"].value;
+    var asunto = document.forms["form"]["asunto"].value;
+    var mensaje = document.forms["form"]["mensaje"].value;
+    
+    if (nombre == "") {
+        alert("Por favor, ingresa tu nombre.");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Por favor, ingresa tu email.");
+        return false;
+    }
+
+    if (asunto == "") {
+        alert("Por favor, ingresa tu asunto.");
+        return false;
+    }
+
+    if (mensaje == "") {
+        alert("Por favor, ingresa tu mensaje.");
+        return false;
+    }
+
+    return true;
+}
